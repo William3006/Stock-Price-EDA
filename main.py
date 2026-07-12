645e805df0924c825b2b84ddaed91982955bd9ac
l <liveciaoliam@gmail.com>
2026-07-12 21:46:14 +0530

    immediate warning added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from data.loader import load_raw, load_new, save
 from features.features import add_return, add_rolling, add_volatility, add_net_profit
-from analysis.anomaly import anomaly_detection
+from analysis.anomaly import anomaly_detection, flag_point_outlier
 from analysis.fattails import show_fattails, add_Correlation_matrix, plot_heatmap
 from analysis.drawdown import drawdown_analysis
 from plots.plots import plot, plot_outlined_data, show_candle_stick
@@ -9,6 +9,4 @@ from plots.plots import plot, plot_outlined_data, show_candle_stick
 
 
 if __name__ == "__main__":
-    mod = load_raw("NP_META_10y")
-
-    
+#####################################################FIX IT FOR WINDOWS############################################
